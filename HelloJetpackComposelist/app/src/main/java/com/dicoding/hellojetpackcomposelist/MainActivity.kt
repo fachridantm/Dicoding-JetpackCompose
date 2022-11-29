53eeed59f376625cdf1ad4bd67bed973dd11d82b
fachridantm <fachridantm@gmail.com>
2022-11-29 12:53:15 +0700

    finish codelab menyusun item

--- HelloJetpackComposelist/app/src/main/java/com/dicoding/hellojetpackcomposelist/MainActivity.kt
+++ HelloJetpackComposelist/app/src/main/java/com/dicoding/hellojetpackcomposelist/MainActivity.kt
@@ -3,13 +3,19 @@ package com.dicoding.hellojetpackcomposelist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dicoding.hellojetpackcomposelist.ui.theme.HelloJetpackComposelistTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +35,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Jetpack Compose Logo",
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Dicoding")
+        }
+    }
 }
 
 @Preview(showBackground = true)
