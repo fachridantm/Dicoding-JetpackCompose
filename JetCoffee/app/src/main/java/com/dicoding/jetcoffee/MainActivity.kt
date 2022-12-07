aa5a628163a8d864a5aedbcceb286f5fe9433516
fachridantm <fachridantm@gmail.com>
2022-12-07 19:02:55 +0700

    latihan bagian menu

--- JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dicoding.jetcoffee.model.Menu
+import com.dicoding.jetcoffee.model.dummyBestSellerMenu
+import com.dicoding.jetcoffee.model.dummyCategory
+import com.dicoding.jetcoffee.model.dummyMenu
+import com.dicoding.jetcoffee.ui.components.CategoryItem
+import com.dicoding.jetcoffee.ui.components.MenuItem
 import com.dicoding.jetcoffee.ui.components.SearchBar
+import com.dicoding.jetcoffee.ui.components.SectionText
 import com.dicoding.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +40,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -50,8 +66,50 @@ fun Banner(
     }
 }
 
-@Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier,
+) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(
+            items = dummyCategory,
+            key = { it.textCategory }
+        ) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier,
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+@Preview(showBackground = true, device = Devices.PIXEL_4)
 fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
         JetCoffeeApp()
