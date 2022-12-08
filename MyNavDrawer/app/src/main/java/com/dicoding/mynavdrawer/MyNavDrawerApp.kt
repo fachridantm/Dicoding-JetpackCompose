f6058078e35c2c798f29395be31462388026d9d2
fachridantm <fachridantm@gmail.com>
2022-12-08 21:54:51 +0700

    latihan membuat nav drawer & side effect handler

--- MyNavDrawer/app/src/main/java/com/dicoding/mynavdrawer/MyNavDrawerApp.kt
+++ MyNavDrawer/app/src/main/java/com/dicoding/mynavdrawer/MyNavDrawerApp.kt
@@ -1,17 +1,27 @@
 package com.dicoding.mynavdrawer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dicoding.mynavdrawer.ui.theme.MyNavDrawerTheme
 import kotlinx.coroutines.launch
 
@@ -19,6 +29,7 @@ import kotlinx.coroutines.launch
 fun MyNavDrawerApp() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -32,8 +43,35 @@ fun MyNavDrawerApp() {
             )
         },
         drawerContent = {
-            Text(stringResource(R.string.hello_from_nav_drawer))
+            MyDrawerContent(
+                onItemSelected = { title ->
+                    scope.launch {
+                        scaffoldState.apply {
+                            drawerState.close()
+                            val snackbarResult = snackbarHostState.showSnackbar(
+                                message = context.resources.getString(R.string.coming_soon, title),
+                                actionLabel = context.resources.getString(R.string.subscribe_question)
+                            )
+                            if (snackbarResult == SnackbarResult.ActionPerformed) {
+                                Toast.makeText(
+                                    context,
+                                    context.resources.getString(R.string.subscribed_info),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                },
+                onBackPressed = {
+                    if (scaffoldState.drawerState.isOpen) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                    }
+                }
+            )
         },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
     ) { paddingValues ->
         Box(
             modifier = Modifier
@@ -42,6 +80,90 @@ fun MyNavDrawerApp() {
             contentAlignment = Alignment.Center,
         ) {
             Text(stringResource(R.string.hello_world))
+        }
+    }
+}
+
+data class MenuItem(val title: String, val icon: ImageVector)
+
+@Composable
+fun MyDrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit,
+    onBackPressed: () -> Unit,
+) {
+    val items = listOf(
+        MenuItem(
+            title = stringResource(R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+    Column(modifier.fillMaxSize()) {
+        Box(
+            modifier = modifier
+                .height(190.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.primary)
+        )
+        for (item in items) {
+            Row(
+                modifier = modifier
+                    .clickable { onItemSelected(item.title) }
+                    .padding(vertical = 12.dp, horizontal = 16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = null,
+                    tint = Color.DarkGray,
+                )
+                Spacer(modifier.width(32.dp))
+                Text(
+                    text = item.title,
+                    style = MaterialTheme.typography.subtitle2,
+                )
+            }
+        }
+        Divider()
+    }
+    BackPressedHandler {
+        onBackPressed()
+    }
+}
+
+@Composable
+fun BackPressedHandler(enabled: Boolean = true, onBackPressed: () -> Unit) {
+    val currentOnBackPressed by rememberUpdatedState(onBackPressed)
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        onDispose {
+            backCallback.remove()
         }
     }
 }
