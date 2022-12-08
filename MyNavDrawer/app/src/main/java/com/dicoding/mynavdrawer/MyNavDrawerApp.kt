79018458a6f40d9d30027799d01e8cfd92fb243a
fachridantm <fachridantm@gmail.com>
2022-12-08 15:11:44 +0700

    latihan membuat nav drawer

--- MyNavDrawer/app/src/main/java/com/dicoding/mynavdrawer/MyNavDrawerApp.kt
+++ MyNavDrawer/app/src/main/java/com/dicoding/mynavdrawer/MyNavDrawerApp.kt
@@ -1,12 +1,68 @@
 package com.dicoding.mynavdrawer
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dicoding.mynavdrawer.ui.theme.MyNavDrawerTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun MyNavDrawerApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
 
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            MyTopBar(
+                onMenuClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
+        },
+        drawerContent = {
+            Text(stringResource(R.string.hello_from_nav_drawer))
+        },
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(stringResource(R.string.hello_world))
+        }
+    }
+}
+
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(stringResource(R.string.app_name))
+        },
+    )
 }
 
 @Preview(showBackground = true)
