9f44bc1f09771fdae7c6018a63acd897d932ae6d
fachridantm <fachridantm@gmail.com>
2022-12-09 11:18:31 +0700

    codelab membuat scroll to top button

---
+++ JetHeroes/app/src/main/java/com/dicoding/jetheroes/JetHeroesApp.kt
@@ -0,0 +1,126 @@
+package com.dicoding.jetheroes
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.dicoding.jetheroes.model.HeroesData.heroes
+import com.dicoding.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun JetHeroesApp(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp),
+        ) {
+            items(heroes, key = { it.id }) { hero ->
+                HeroListItem(name = hero.name,
+                    photoUrl = hero.photoUrl,
+                    modifier = Modifier.fillMaxWidth())
+            }
+        }
+        AnimatedVisibility(visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.animateScrollToItem(0)
+                    }
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier.clickable { }) {
+        AsyncImage(model = photoUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape))
+        Text(text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(start = 16.dp))
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary)) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(name = "H.O.S.Cokroaminoto", photoUrl = "")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesAppPreview() {
+    JetHeroesTheme {
+        JetHeroesApp()
+    }
+}
--- JetHeroes/app/src/main/java/com/dicoding/jetheroes/MainActivity.kt
+++ JetHeroes/app/src/main/java/com/dicoding/jetheroes/MainActivity.kt
@@ -18,24 +18,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetHeroesTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    JetHeroesApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetHeroesTheme {
-        Greeting("Android")
     }
 }
