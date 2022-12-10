f56c947372b48b8b2cece927fc5993afa97addaa
fachridantm <fachridantm@gmail.com>
2022-12-10 21:29:40 +0700

    latihan nav component dgn bottom nav

--- JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,38 +1,63 @@
 package com.dicoding.jetreward
 
-import android.graphics.drawable.Icon
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
+import com.dicoding.jetreward.ui.screen.cart.CartScreen
+import com.dicoding.jetreward.ui.screen.home.HomeScreen
+import com.dicoding.jetreward.ui.screen.profile.ProfileScreen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
 ) {
-
+    Scaffold(
+        bottomBar = { BottomBar(navController) },
+        modifier = modifier,
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.Cart.route) { CartScreen() }
+            composable(Screen.Profile.route) { ProfileScreen() }
+        }
+    }
 }
 
 @Composable
 private fun BottomBar(
+    navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     BottomNavigation(
         modifier = modifier,
     ) {
         val navigationItem = listOf(
-            NavigationItem(title = stringResource(id = R.string.menu_home),
+            NavigationItem(title = stringResource(R.string.menu_home),
                 icon = Icons.Default.Home,
                 screen = Screen.Home),
             NavigationItem(title = stringResource(R.string.menu_cart),
@@ -44,16 +69,26 @@ private fun BottomBar(
         )
         BottomNavigation {
             navigationItem.map { item ->
-                BottomNavigationItem(icon = { Icon(item.icon, contentDescription = item.title) },
+                BottomNavigationItem(
+                    icon = { Icon(item.icon, contentDescription = item.title) },
                     label = { Text(text = item.title) },
-                    selected = true,
-                    onClick = { })
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun JetHeroesAppPreview() {
     JetRewardTheme {
