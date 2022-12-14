e069dee67f9efc6dd249c61711384f4fd5c2aca3
fachridantm <fachridantm@gmail.com>
2022-12-15 01:38:36 +0700

    create dummy model

---
+++ BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/model/FakeMerchDataSource.kt
@@ -0,0 +1,18 @@
+package com.dicoding.bangkitmerchstore.model
+
+import com.dicoding.bangkitmerchstore.R
+
+object FakeMerchDataSource {
+    val dummyMerch = listOf(
+        Merch(1, R.drawable.bangkit_earbuds, "Exclusive earbuds", 6000),
+        Merch(2, R.drawable.bangkit_jackets, "Jacket", 4500),
+        Merch(3, R.drawable.bangkit_headset, "Pouch with items inside", 1000),
+        Merch(4, R.drawable.bangkit_tumblr, "Digital Tumblr", 1000),
+        Merch(5, R.drawable.bangkit_tshirt, "T-shirt", 1000),
+        Merch(6, R.drawable.bangkit_mug, "Mug", 1000),
+        Merch(7, R.drawable.bangkit_pouch, "Pouch", 750),
+        Merch(8, R.drawable.bangkit_notebook, "Notebook", 750),
+        Merch(9, R.drawable.bangkit_stickers, "Stickerset", 300),
+        Merch(10, R.drawable.tokopedia_gift_card, "Tokopedia Gift Card - 50k", 300),
+    )
+}
