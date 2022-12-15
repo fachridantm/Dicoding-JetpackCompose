a01dfa5287287b1dce1794ea4eb319a6a03d5494
fachridantm <fachridantm@gmail.com>
2022-12-15 11:18:03 +0700

    add center alignment for empty cart

--- BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/ui/screen/cart/CartScreen.kt
+++ BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/ui/screen/cart/CartScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -58,6 +59,7 @@ fun CartScreen(
 @Composable
 fun EmptyCart() {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxSize()
