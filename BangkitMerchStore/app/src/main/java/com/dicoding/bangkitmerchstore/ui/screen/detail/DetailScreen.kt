a39ac8f5f0b1bafbd2957baab60c0c0b389887b0
fachridantm <fachridantm@gmail.com>
2022-12-15 08:41:11 +0700

    create DetailScreen.kt

---
+++ BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/ui/screen/detail/DetailScreen.kt
@@ -0,0 +1,173 @@
+package com.dicoding.bangkitmerchstore.ui.screen.detail
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dicoding.bangkitmerchstore.R
+import com.dicoding.bangkitmerchstore.di.Injection
+import com.dicoding.bangkitmerchstore.ui.ViewModelFactory
+import com.dicoding.bangkitmerchstore.ui.common.StateHolder
+import com.dicoding.bangkitmerchstore.ui.components.OrderButton
+import com.dicoding.bangkitmerchstore.ui.components.ProductCounter
+import com.dicoding.bangkitmerchstore.ui.theme.BangkitMerchStoreTheme
+
+@Composable
+fun DetailScreen(
+    merchId: Long,
+    viewModel: DetailMerchViewModel = viewModel(
+        factory = ViewModelFactory(
+            Injection.provideRepository()
+        )
+    ),
+    navigateBack: () -> Unit,
+    navigateToCart: () -> Unit,
+) {
+    viewModel.stateHolder.collectAsState(initial = StateHolder.Loading).value.let { stateHolder ->
+        when (stateHolder) {
+            is StateHolder.Loading -> {
+                viewModel.getMerchById(merchId)
+            }
+            is StateHolder.Success -> {
+                val data = stateHolder.data
+                DetailContent(
+                    data.merch.image,
+                    data.merch.title,
+                    data.merch.requiredPoint,
+                    data.count,
+                    onBackClick = navigateBack,
+                    onAddToCart = { count ->
+                        viewModel.addToCart(data.merch, count)
+                        navigateToCart()
+                    }
+                )
+            }
+            is StateHolder.Error -> {}
+        }
+    }
+}
+
+@Composable
+fun DetailContent(
+    @DrawableRes image: Int,
+    title: String,
+    basePoint: Int,
+    count: Int,
+    onBackClick: () -> Unit,
+    onAddToCart: (count: Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var totalPoint by rememberSaveable { mutableStateOf(0) }
+    var orderCount by rememberSaveable { mutableStateOf(count) }
+
+    Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f)
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = modifier
+                        .height(400.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.back),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { onBackClick() }
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = title,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.h5.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                )
+                Text(
+                    text = stringResource(R.string.required_point, basePoint),
+                    style = MaterialTheme.typography.subtitle1.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                    color = MaterialTheme.colors.secondary
+                )
+                Text(
+                    text = stringResource(R.string.lorem_ipsum),
+                    style = MaterialTheme.typography.body2,
+                    textAlign = TextAlign.Justify,
+                )
+            }
+        }
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(4.dp)
+            .background(LightGray))
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ProductCounter(
+                1,
+                orderCount,
+                onProductIncreased = { orderCount++ },
+                onProductDecreased = { if (orderCount > 0) orderCount-- },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 16.dp)
+            )
+            totalPoint = basePoint * orderCount
+            OrderButton(
+                text = stringResource(R.string.add_to_cart, totalPoint),
+                enabled = orderCount > 0,
+                onClick = {
+                    onAddToCart(orderCount)
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun DetailContentPreview() {
+    BangkitMerchStoreTheme {
+        DetailContent(
+            R.drawable.bangkit_tumblr,
+            "Digital Tumblr",
+            1500,
+            1,
+            onBackClick = {},
+            onAddToCart = {}
+        )
+    }
+}
