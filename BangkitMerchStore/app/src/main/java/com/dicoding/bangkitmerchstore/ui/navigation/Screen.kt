4f7dd3c767aaf0f8e25cf71cbd3c38caa38b86e9
fachridantm <fachridantm@gmail.com>
2022-12-15 09:11:14 +0700

    refactoring route name

--- BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/ui/navigation/Screen.kt
+++ BangkitMerchStore/app/src/main/java/com/dicoding/bangkitmerchstore/ui/navigation/Screen.kt
@@ -4,7 +4,7 @@ sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Cart : Screen("cart")
     object Profile : Screen("profile")
-    object DetailReward : Screen("home/{merchId}") {
+    object DetailMerch : Screen("home/{merchId}") {
         fun createRoute(merchId: Long) = "home/$merchId"
     }
 }
